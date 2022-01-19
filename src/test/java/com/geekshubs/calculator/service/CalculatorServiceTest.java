5af203e0a4cd1ff193c644fdb8556a85c0d13293
Ricardo García Fernández <ricardogarfe@gmail.com>
2022-01-19 13:22:31 +0100

    feat: Extract service instance

--- src/test/java/com/geekshubs/calculator/service/CalculatorServiceTest.java
+++ src/test/java/com/geekshubs/calculator/service/CalculatorServiceTest.java
@@ -6,23 +6,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorServiceTest {
 
+    CalculatorService calculatorService = new CalculatorService();
+
     @Test
     public void testAdd() {
-        assertEquals(34, new CalculatorService().Add(8, 26).getResult());
+        assertEquals(34, calculatorService.Add(8, 26).getResult());
     }
 
     @Test
     public void testSub() {
-        assertEquals(4, new CalculatorService().Sub(12, 8).getResult());
+        assertEquals(4, calculatorService.Sub(12, 8).getResult());
     }
 
     @Test
     public void testMul() {
-        assertEquals(88, new CalculatorService().Mul(11, 8).getResult());
+        assertEquals(88, calculatorService.Mul(11, 8).getResult());
     }
 
     @Test
     public void testDiv() {
-        assertEquals(1, new CalculatorService().Div(12, 12).getResult());
+        assertEquals(1, calculatorService.Div(12, 12).getResult());
     }
 }
