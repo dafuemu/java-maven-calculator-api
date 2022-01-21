7719f05188ac6bfc1649e284a0a0ebcfe1dc9682
davidfuentes <david.fuentes.sge@mango.com>
2022-01-21 18:12:39 +0100

    tests and ci

--- src/main/java/com/geekshubs/calculator/Calculator.java
+++ src/main/java/com/geekshubs/calculator/Calculator.java
@@ -2,4 +2,23 @@ package com.geekshubs.calculator;
 
 public class Calculator {
 
+    public int sum(int i, int j) {
+        return i + j;
+    }
+
+    public int res(int i, int j) {
+        return i - j;
+    }
+
+    public int mul(int i, int j) {
+        return i * j;
+    }
+
+    public int div(int i, int j) {
+        if (j == 0) {
+            throw new ArithmeticException("Cannot divide by 0");
+        }
+        return i / j;
+    }
+
 }
--- src/test/java/com/geekshubs/calculator/CalculatorTest.java
+++ src/test/java/com/geekshubs/calculator/CalculatorTest.java
@@ -1,5 +1,50 @@
 package com.geekshubs.calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
+
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int result = calculator.sum(1, 2);
+
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void testRes() {
+        Calculator calculator = new Calculator();
+        int result = calculator.res(2, 1);
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testMul() {
+        Calculator calculator = new Calculator();
+        int result = calculator.mul(2, 1);
+
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testDiv() {
+        Calculator calculator = new Calculator();
+        int result = calculator.div(2, 1);
+
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testDivByZero() {
+        Calculator calculator = new Calculator();
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            calculator.div(2, 0);
+        });
+    }
 
 }
